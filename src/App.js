78ed49e582b95eef7d6aaade746b4ab83c383cf1
raaj5739 <rajkumar7862001@gmail.com>
2024-01-22 13:04:53 +0530

    hhh

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import './App.css';
 import Nevbar from './Components/Nevbar';
 import TextForm from './Components/TextForm';
-// import Alert from './Components/Alert';
+import Alert from './Components/Alert';
 // import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 // import About from './Components/About';
@@ -10,19 +10,19 @@ import TextForm from './Components/TextForm';
 // import styles from'./public.button.css'; // Import the CSS file
 
 
-function App() {{
+function App() {
 
 const [mode, setMode]= useState('light');
 // const [alert, setAlert]=useState(null);
 
-// const showAlert = (message,type) => {
-//   setAlert({
-//     msg: message,
-//     type: type,
-  // })
-  // setTimeout(() => {
-  //   showAlert(null);
-  // }, 30000);
+const showAlert = (message,type) => {
+  setAlert({
+    msg: message,
+    type: type,
+  })
+  setTimeout(() => {
+    setAlert(null);
+  }, 30000);
 
 };
 
@@ -31,12 +31,12 @@ const toggleMode = () => {
     setMode("dark");
     document.body.style.backgroundColor = "black";
     document.body.style.color = "white";
-    // showAlert("darkmode has been enabled","success");
+    showAlert("darkmode has been enabled","success");
   } else {
     setMode("light");
     document.body.style.backgroundColor = "white";
     document.body.style.color = "black";
-    // showAlert("darkmode has been desabled","success");
+    showAlert("darkmode has been desabled","success");
 
   }
 };
@@ -50,7 +50,7 @@ return (
       mode={mode}
       toggleMode={toggleMode}
     />
-    {/* <Alert alert={alert} /> */}
+    <Alert alert={alert} />
     <div className="container ">
       <TextForm showAlert={showAlert} heading="Enter the text here" /> 
       {/* <About/> */}
